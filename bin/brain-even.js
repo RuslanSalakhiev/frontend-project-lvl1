526f6d1a428e8ebd51abc09dfb0d2d6e48784141
rs <salakhiev.ruslan@gmail.com>
2022-04-29 21:51:27 +0300

    remove functions from bin to src

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,54 +1,21 @@
 #!/usr/bin/env node
+import * as be from '../src/brain-even.js';
 
-import readlineSync from 'readline-sync';
-
-function greeting() {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  return name;
-}
-
-function getRandomNum() {
-  return Math.round(Math.random() * 10);
-}
-
-function askQuestion(questionNum) {
-  console.log(`Question: ${questionNum}`);
-
-  return readlineSync.question('Your answer: ');
-}
-
-function getCorrectAnswer(questionNum) {
-  return questionNum % 2 === 0 ? 'yes' : 'no';
-}
-
-function correctReply() {
-  console.log('Correct!');
-}
-
-function incorrectReply(actualAnswer, correctAnswer, name) {
-  console.log(`'${actualAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-  console.log(`Let's try again, ${name}!`);
-}
-
-// Start
 let counts = 1;
-const name = greeting();
+const name = be.greeting();
 let isCorrectAnswer;
 
 do {
-  const randomNum = getRandomNum();
-  const answer = askQuestion(randomNum);
-  const correctAnswer = getCorrectAnswer(randomNum);
+  const randomNum = be.getRandomNum();
+  const answer = be.askQuestion(randomNum);
+  const correctAnswer = be.getCorrectAnswer(randomNum);
   isCorrectAnswer = answer === correctAnswer;
 
   if (isCorrectAnswer) {
-    correctReply();
+    be.correctReply();
     counts += 1;
   } else {
-    incorrectReply(answer, correctAnswer, name);
+    be.incorrectReply(answer, correctAnswer, name);
     break;
   }
 } while (isCorrectAnswer && counts <= 3);
