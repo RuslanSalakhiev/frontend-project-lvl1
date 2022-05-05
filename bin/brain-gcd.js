57ec90c000cba0190a7dba065615b4ab4b0cea35
rs <salakhiev.ruslan@gmail.com>
2022-05-05 18:01:49 +0300

    small fix

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import startGame from '../src/index.js';
+import { startGame } from '../src/index.js';
 
 const gameName = 'brainGCD';
 const gameRules = 'Find the greatest common divisor of given numbers.';
